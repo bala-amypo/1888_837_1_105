361377ded734b6d9528f6740f9449378a32d5b08
bala-amypo <bala@amypo.in>
2025-12-21 06:23:09 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
@@ -1,5 +1,15 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Visitor;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.VisitorRepository;
+import com.example.demo.service.VisitorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
-public class VisitorServiceImpl {
+public class VisitorServiceImpl implements VisitorService {
 
     private final VisitorRepository visitorRepository;
 
@@ -7,12 +17,20 @@ public class VisitorServiceImpl {
         this.visitorRepository = visitorRepository;
     }
 
-    public Visitor createVisitor(Visitor v) {
-        return visitorRepository.save(v);
+    @Override
+    public Visitor createVisitor(Visitor visitor) {
+        return visitorRepository.save(visitor);
     }
 
+    @Override
     public Visitor getVisitor(Long id) {
         return visitorRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Visitor not found"));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Visitor not found"));
+    }
+
+    @Override
+    public List<Visitor> getAllVisitors() {
+        return visitorRepository.findAll();
     }
 }
