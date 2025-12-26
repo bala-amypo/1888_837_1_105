8c7afb5625c190b6cc62a4d10e39d64ab260100a
bala-amypo <bala@amypo.in>
2025-12-26 07:57:46 +0000

    User commit

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,11 +1,16 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Appointment;
+
 import java.util.List;
 
 public interface AppointmentService {
 
-    Appointment createAppointment(Appointment appointment);
+    Appointment createAppointment(Long visitorId, Long hostId, Appointment appointment);
 
-    List<Appointment> getAllAppointments();
+    Appointment getAppointment(Long id);
+
+    List<Appointment> getAppointmentsForHost(Long hostId);
+
+    List<Appointment> getAppointmentsForVisitor(Long visitorId);
 }
--- src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
@@ -1,16 +1,15 @@
-package com.example.demo.serviceimpl;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.Visitor;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.VisitorRepository;
 import com.example.demo.service.VisitorService;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class VisitorServiceImpl implements VisitorService {
 
-    private final VisitorRepository visitorRepository;
+    private VisitorRepository visitorRepository;
 
     public VisitorServiceImpl(VisitorRepository visitorRepository) {
         this.visitorRepository = visitorRepository;
@@ -19,6 +18,12 @@ public class VisitorServiceImpl implements VisitorService {
     @Override
     public Visitor createVisitor(Visitor visitor) {
         return visitorRepository.save(visitor);
+    }
+
+    @Override
+    public Visitor getVisitor(Long id) {
+        return visitorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Visitor not found"));
     }
 
     @Override
