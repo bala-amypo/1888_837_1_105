9431167ba59db4ac74f40a2db4de3f0d39bdac76
bala-amypo <bala@amypo.in>
2025-12-26 09:03:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/AppointmentController.java
+++ src/main/java/com/example/demo/controller/AppointmentController.java
@@ -1,3 +1,11 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Appointment;
+import com.example.demo.service.AppointmentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
