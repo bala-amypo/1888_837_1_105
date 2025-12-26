70748ea1c6bfcafcba5bfcfece575f360a9d3b95
bala-amypo <bala@amypo.in>
2025-12-26 06:06:46 +0000

    User commit

--- src/main/java/com/example/demo/service/HostService.java
+++ src/main/java/com/example/demo/service/HostService.java
@@ -4,7 +4,8 @@ import com.example.demo.entity.Host;
 import java.util.List;
 
 public interface HostService {
+
     Host createHost(Host host);
-    Host getHost(Long id);
+
     List<Host> getAllHosts();
 }
--- src/main/java/com/example/demo/serviceimpl/HostServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/HostServiceImpl.java
@@ -1,12 +1,13 @@
-package com.example.demo.service.impl;
+package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.Host;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.HostRepository;
 import com.example.demo.service.HostService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class HostServiceImpl implements HostService {
 
     private final HostRepository hostRepository;
@@ -18,12 +19,6 @@ public class HostServiceImpl implements HostService {
     @Override
     public Host createHost(Host host) {
         return hostRepository.save(host);
-    }
-
-    @Override
-    public Host getHost(Long id) {
-        return hostRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Host not found"));
     }
 
     @Override
