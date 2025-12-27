1d870956830837384f2bc0c273660f06fd3f2cf7
bala-amypo <bala@amypo.in>
2025-12-27 04:14:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertNotificationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertNotificationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.AlertNotification;
 import com.example.demo.model.VisitLog;
 import com.example.demo.repository.AlertNotificationRepository;
@@ -26,11 +27,22 @@ public class AlertNotificationServiceImpl implements AlertNotificationService {
 
     @Override
     public AlertNotification sendAlert(Long visitLogId) {
-        VisitLog visitLog = visitLogRepository.findById(visitLogId).orElse(null);
+
+        VisitLog visitLog = visitLogRepository.findById(visitLogId)
+                .orElseThrow(() -> new ResourceNotFoundException("VisitLog not found"));
+
+        if (!visitLog.isCheckedIn()) {
+            throw new IllegalStateException("Cannot send alert before check-in");
+        }
+
+        if (alertRepository.existsByVisitLog(visitLog)) {
+            throw new IllegalArgumentException("Duplicate alert");
+        }
 
         AlertNotification alert = new AlertNotification();
         alert.setVisitLog(visitLog);
         alert.setAlertMessage("Visitor arrived");
+        alert.setSent(true);
         alert.setSentAt(LocalDateTime.now());
 
         return alertRepository.save(alert);
@@ -38,7 +50,8 @@ public class AlertNotificationServiceImpl implements AlertNotificationService {
 
     @Override
     public AlertNotification getAlert(Long id) {
-        return alertRepository.findById(id).orElse(null);
+        return alertRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Alert not found"));
     }
 
     @Override
